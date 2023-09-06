45ac78787e421d6844a738647f8a1f1bde2282b0
Keaton K <keatonkupillas@yahoo.com>
2023-09-06 11:38:06 -0700

    added WebAppTutorial

---
+++ wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿
+const vidContainer = document.getElementById('vidContainer');
+let isYellow = true;
+
+    vidContainer.addEventListener('click', () => {
+        if (isYellow) {
+    vidContainer.style.background = 'blue';
+        } else {
+    vidContainer.style.background = 'yellow';
+        }
+isYellow = !isYellow;
+    });
